91e638debdc001621b7afe8a5201a10c78e80ee3
Mike Treanor <treanor@american.edu>
2024-04-15 23:06:41 -0400

    finished cookie example

--- examples/4-15-24/Cookie.js
+++ examples/4-15-24/Cookie.js
@@ -1,41 +1,46 @@
 class Cookie {
     constructor(x, y) {
         this.pos = createVector(x, y);
-        this.vel = createVector(random(-1, 1), random(-1, 1));
+        this.vel = createVector(random(1, 2), random(1, 2));
+        this.vel.x *= (random() > 0.5) ? -1: 1;
+        this.vel.y *= (random() > 0.5) ? -1: 1;
 
-        this.size = random(10, 100);
+        this.size = random(0.8, 1.2);
 
-        this.hue = 100;
+        this.hue = random(360);
 
         this.currentZone = this.computeZone();
 
-        this.osc = new p5.Oscillator('sine');
-        this.osc.start();
+        this.thetaOffset = random(2 * PI);
+
     }
 
     computeZone() {
-        let zone = floor(map(this.pos.x, 0, width, 0, scales[scale].length));
+        let zone = floor(map(this.pos.x, 0, width, 0, scales[whichScale].length));
         return zone;
     }
 
     update() {
+        if (this.osc === undefined) {
+            let oscType = random(["sine", "triangle", "square", "sawtooth"]);
+            this.osc = new p5.Oscillator(oscType);
+            reverb.process(this.osc, 10, 2);
+            this.osc.start();
+        }
+        this.osc.amp(min(0.7, abs(sin(this.thetaOffset + frameCount * 0.01))));
+
         this.pos.add(this.vel);
         if (this.pos.x > width || this.pos.x < 0) {
             this.vel.x *= -1;
-            let note = root;
-            // synth.play(midiToFreq(note), 0.8, 0, 0.1);
         }   
         if (this.pos.y > height || this.pos.y < 0) {
             this.vel.y *= -1;
-            // let note = root + floor(map(this.pos.x, 0, width, 0, scales[scale].length));
-            // note += floor(map(this.size, 100, 10, -2, 2)) * 12;
-            // synth.play(midiToFreq(note), 0.8, 0, 0.1);
         }
 
         let zone = this.computeZone();
         if (zone !== this.currentZone) {
-            let note = root + floor(map(this.pos.x, 0, width, 0, scales[scale].length));
-            note += floor(map(this.size, 100, 10, -2, 2)) * 12;
+            let note = root + floor(map(this.pos.x, 0, width, 0, scales[whichScale].length));
+            note += floor(map(this.size, 1.2, 0.8, -2, 2)) * 12;
             synth.play(midiToFreq(note), 0.8, 0, 0.1);
 
             this.osc.freq(midiToFreq(note));
@@ -44,8 +49,11 @@ class Cookie {
         }
     }
     show() {
-        fill(this.hue, 60, 100);
-        ellipse(this.pos.x, this.pos.y, this.size);
-        // image(cookieImage, this.pos.x, this.pos.y);
+        push();
+        translate(this.pos.x, this.pos.y);
+        tint(this.hue, 50, 100);
+        scale(this.size);
+        image(cookieImage, 0, 0);
+        pop();
     }
 }
--- examples/4-15-24/sketch.js
+++ examples/4-15-24/sketch.js
@@ -6,22 +6,25 @@ let reverb;
 let sixteenth = 0;
 
 let root = 48;
-let scale = "major";
+let whichScale = "pentatonic major";
 
 let hue;
 
-// let loopInterval = 1; // Loop interval of 1 second corresponds to 60 BPM
+let loopInterval = 1/4; // Loop interval of 1 second corresponds to 60 BPM
 
 let cookies = [];
-let numCookies = 4;
+let numCookies = 3;
 
 let cookieImg;
+let monsterImg;
 
 let playing = false;
 
 function preload() {
   kickSample = loadSound("./samples/kick.wav");
   cookieImage = loadImage("./images/cookie.png");
+  monsterImg = loadImage("./images/monster.png");
+
 }
 
 
@@ -33,11 +36,10 @@ function setup() {
   cookieImage.resize(100, 0);
 
   for (let i = 0; i < numCookies; i++) {
-    cookies.push(new Cookie(width/2, height/2));
+    cookies.push(new Cookie(random(width), height/2));
   }
 
   synth = new p5.PolySynth();
-  // synth.maxVoices = 160;
 
   reverb = new p5.Reverb();
   reverb.process(synth, 10, 2);
@@ -45,26 +47,41 @@ function setup() {
   delay = new p5.Delay();
   delay.process(synth, 0.4, 0.2, 2300);
 
-  // loop = new p5.SoundLoop(soundLoop, loopInterval/4);
+  loop = new p5.SoundLoop(soundLoop, loopInterval);
+
+  background(0, 0, 100);
 }
 
 function draw() {
-  background(0, 0, 100);
+  background(0, 0, 100, 0.01);
 
 
-  for (let i = 0; i < cookies.length; i++) {
-    cookies[i].update();
-    cookies[i].show();
+  if (playing) {
+    push();
+    translate(width/2, height);
+    scale(0.5 + noise(100 + frameCount * 0.001) * 1);
+    rotate(noise(frameCount * 0.001) * 2 * PI - PI);
+    image(monsterImg, 0, 0);
+    pop();
+
+    for (let i = 0; i < cookies.length; i++) {
+      cookies[i].update();
+      cookies[i].show();
+    }
+  } else {
+    // textMode(CENTER);
+    text("Warning, turn down your sound!\nClick at your own risk...", width/2 - 100, height/2);
   }
 
 }
 
-// function soundLoop(timeFromNow) {
-//   let note = root;
-//   synth.play(midiToFreq(note), 0.8, timeFromNow, 0.1);
+function soundLoop(timeFromNow) {
+  if (sixteenth % 2 === 0) {
+    kickSample.play(timeFromNow);
+  }
 
-//   sixteenth++;
-// }
+  sixteenth++;
+}
 
 function mousePressed() {
   if (!playing) {
