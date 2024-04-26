1dd78280647e260d5b2c358b3b0fa3730854786a
Mike Treanor <treanor@american.edu>
2024-04-25 20:57:00 -0400

    Update sketch.js

--- examples/4-22-24/sketch.js
+++ examples/4-22-24/sketch.js
@@ -1,24 +1,28 @@
 let img;
-let imgWidth = 20;
-let imgHeight = 14;
+let imgWidth = 40;
+let imgHeight = 28;
 
-function preload() {
-  img = loadImage("./frog.png"); // 20 by 14
-}
+let pixelDimension;
+
+// function preload() {
+   // img = loadImage("./frog.png"); // 20 by 14
+// }
 
 function setup() {
   createCanvas(600, 400);
   
-  // img = createCapture(VIDEO);
-  // img.size(imgWidth, imgHeight);
-  // img.hide();
+  img = createCapture(VIDEO);
+  img.size(imgWidth, imgHeight);
+  img.hide();
+
+  pixelDimension = height / imgHeight;
 
   pixelDensity(1);
   background(0, 0, 0);
 }
 
 function draw() {
-  background(0, 0, 0, 10);
+  background(0, 0, 0);
 
   img.loadPixels();
   for (let y = 0; y < img.height; y++) {
@@ -31,16 +35,16 @@ function draw() {
 
       let avg = (r+g+b) / 3;
       let alpha = 255;
-      // if (avg > 100) {
+      // if (avg > 120) {
       //   alpha = 0;
       // }
 
       fill(r, g, b, alpha);
       let offsetX = map(x, 0, 20, 0, 2 * PI);
-      let xPos = x*30 + cos(offsetX + frameCount * 0.1) * 2
+      let xPos = x*pixelDimension + cos(offsetX + frameCount * 0.1) * 2
       let offsetY = map(y, 0, 14, 0, 2 * PI);
-      let yPos = y*30 + sin(offsetY + frameCount * 0.1) * 2
-      ellipse(xPos, yPos, 30);
+      let yPos = y*pixelDimension + sin(offsetY + frameCount * 0.1) * 2
+      ellipse(xPos, yPos, pixelDimension);
     }
   }
 }
